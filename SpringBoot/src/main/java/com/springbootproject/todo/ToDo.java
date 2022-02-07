f6a1d4a30d9cc3b46d66941d146c08a9af42420b
Roman Koenig <rk.home42@gmail.com>
2022-02-07 16:46:49 +0100

    Aufgabe32 initial commit

---
+++ SpringBoot/src/main/java/com/springbootproject/todo/ToDo.java
@@ -0,0 +1,41 @@
+package com.springbootproject.todo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="TODO")
+public class ToDo {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+
+    @Column(name="TODO_TASK", length=100, nullable=false, unique=false)
+    private String task;
+    
+    //Constructor
+    public ToDo() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    
+}
